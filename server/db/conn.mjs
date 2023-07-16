b8f427164e4d45977c4846bce77e5aaa8bbc13ce
Keg Avakian <kjavakian@outlook.com>
2023-07-16 17:53:56 -0500

    initial backend commit

---
+++ server/db/conn.mjs
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.ATLAS_URI || "";
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+  conn = await client.connect();
+} catch (err) {
+  console.error(err);
+}
+
+let db = conn.db("testing");
+
+export default db;
---
+++ server/server.mjs
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import records from "./routes/record.mjs";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
